33247611cfd9f18c5cc495bf6907b1ac057f5c29
Saulinho55 <Saulespino555@gmail.com>
2025-05-11 00:01:01 +0100

    Proyecto Spring Creado
    Proyecto Spring Creado

---
+++ librosyautores/src/main/java/com/api/librosyautores/LibrosyautoresApplication.java
@@ -0,0 +1,13 @@
+package com.api.librosyautores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosyautoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosyautoresApplication.class, args);
+	}
+
+}
